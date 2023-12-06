cc3841bac82ca91bd934775f27e371c458de2a52
Sakolkrit Pengkhum <93993627+Sakolkrit@users.noreply.github.com>
2023-12-06 15:45:49 +0700

    fix: Add navbar component into page.tsx

--- devath-frontend/src/app/page.tsx
+++ devath-frontend/src/app/page.tsx
@@ -1,5 +1,10 @@
+"use client"; /* I got error if I not type this one on top of the page */
 import { Heading, Text } from "@chakra-ui/react";
+import Navbar from "../components/navbar/navbar";
 
 export default function Home() {
-  return <Heading color="brand.primary">Hey!</Heading>;
+  return <Heading color="brand.primary">
+    <Navbar/> {/* add Navbar here*/}
+    {/* aadd context here*/}
+  </Heading>;
 }
