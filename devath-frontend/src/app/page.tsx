c4e80599eb815eef69a6422f7030d3c914b7a4ea
quadiez <dduidokdoi@gmail.com>
2023-12-11 13:00:46 +0700

    feat: responsive

--- devath-frontend/src/app/page.tsx
+++ devath-frontend/src/app/page.tsx
@@ -1,10 +1,122 @@
-"use client"; /* I got error if I not type this one on top of the page */
-import { Heading, Text } from "@chakra-ui/react";
+import { Heading, Stack, Text } from "@chakra-ui/react";
 import Navbar from "../components/navbar/navbar";
+import { getAllFounders, getAllFoundersData } from "@/utils";
+import startCase from "lodash/startCase";
+import { DevaTag, FounderDisplayData, FoundersList } from "@/components";
+import { ContactSection } from "@/components/contact/ContactSection";
 
-export default function Home() {
-  return <Heading color="brand.primary">
-    <Navbar/> {/* add Navbar here*/}
-    {/* aadd context here*/}
-  </Heading>;
+export default async function Home() {
+  const allFounders = await getAllFoundersData().map(
+    (founder) =>
+      ({
+        name: founder.name,
+        nickname: startCase(founder.nickname),
+        pictureSrc: `/founders/img/${founder.name}.png`,
+        position: founder.position,
+      } as FounderDisplayData)
+  );
+
+  return (
+    <Stack>
+      <Navbar />
+      <Stack
+        pos="absolute"
+        top="0"
+        right="0"
+        gap="0"
+        bgColor="background.primary"
+      >
+        <Stack
+          w="100vw"
+          h="100vh"
+          top="0"
+          right="0"
+          justify="center"
+          bgColor="background.primary"
+          color="content.primary"
+          px={[10, 16]}
+          pt={20}
+          pb={20}
+          gap={8}
+        >
+          <Text
+            fontSize={["6.5vh", "8vh", "6vw"]}
+            fontWeight="300"
+            lineHeight="1.2"
+          >
+            Never doubt that small group of thoughtful, committed citizens can
+            change the world.
+          </Text>
+        </Stack>
+
+        <Stack
+          w="100vw"
+          h="100vh"
+          top="0"
+          right="0"
+          bgColor="background.primary"
+          color="content.primary"
+          px={[10, 16]}
+          pt={12}
+          pb={20}
+          gap={8}
+          justify="center"
+        >
+          <DevaTag label="About" />
+          <Stack direction={["column", "column", "row"]} gap={[8, 8, 16]}>
+            <Stack>
+              <Text fontSize={["4xl", "4xl", "5xl", "6xl"]} fontWeight="300">
+                Doing the things we do every day a little bit better.
+              </Text>
+            </Stack>
+            <Stack gap={6} pt={[0, 0, 4]}>
+              <Text fontSize={["md", "md", "xl"]}>
+                Welcome to the forefront of innovation! 🚀 We are "DevA",
+                Developer and Agile Thinker. Aspiring young entrepreneurs to
+                create innovation to change the world.
+              </Text>
+              <Text>
+                We're a team of passionate innovators dedicated to creating a
+                better world through the latest trends in design, technology,
+                and business. Follow us for a fast-paced, up-to-date journey
+                into the exciting realms of innovation.
+              </Text>
+            </Stack>
+          </Stack>
+        </Stack>
+        <Stack
+          w="100vw"
+          h="100vh"
+          top="0"
+          right="0"
+          bgColor="background.primary"
+          color="content.primary"
+          px={[10, 16]}
+          pt={12}
+          pb={20}
+          gap={8}
+        >
+          <DevaTag label="Project" />
+        </Stack>
+        <Stack
+          w="100vw"
+          h="100vh"
+          top="0"
+          right="0"
+          bgColor="background.primary"
+          color="content.primary"
+          px={[10, 16]}
+          pt={12}
+          pb={20}
+          gap={8}
+        >
+          <DevaTag label="Founder" />
+          <Stack>
+            <FoundersList items={allFounders} />
+          </Stack>
+        </Stack>
+        <ContactSection />
+      </Stack>
+    </Stack>
+  );
 }
--- devath-frontend/src/components/navbar/navbar.tsx
+++ devath-frontend/src/components/navbar/navbar.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 // Navbar.tsx
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import {
   Box,
   Flex,
@@ -14,8 +16,15 @@ import {
   PopoverContent,
   useColorModeValue,
   useDisclosure,
-} from '@chakra-ui/react';
-import { HamburgerIcon, CloseIcon, ChevronDownIcon, ChevronRightIcon } from '@chakra-ui/icons';
+} from "@chakra-ui/react";
+import {
+  HamburgerIcon,
+  CloseIcon,
+  ChevronDownIcon,
+  ChevronRightIcon,
+} from "@chakra-ui/icons";
+import { readdirSync } from "fs";
+import { getAllFounders } from "@/utils";
 
 const Navbar: React.FC = () => {
   const { isOpen, onToggle } = useDisclosure();
@@ -27,63 +36,76 @@ const Navbar: React.FC = () => {
       const currentScrollPos = window.pageYOffset;
 
       setVisible(
-        (prevScrollPos > currentScrollPos && prevScrollPos - currentScrollPos > 70) || currentScrollPos < 10
+        (prevScrollPos > currentScrollPos &&
+          prevScrollPos - currentScrollPos > 70) ||
+          currentScrollPos < 10
       );
 
       setPrevScrollPos(currentScrollPos);
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-    return () => window.removeEventListener('scroll', handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, [prevScrollPos]);
 
   return (
     <Box
       position="fixed"
-      top="0"
       width="100%"
       zIndex="1000"
       transition="top 0.3s"
-      top={visible ? '0' : '-60px'}
+      top={visible ? "0" : "-60px"}
     >
       <Flex
-        bg={useColorModeValue('#111111', '#FFFFFF')} //block color 
-        color={useColorModeValue('#FFFFFF', '#111111')} //the alphabet color inside block 
-        minH={'60px'}
+        bg={useColorModeValue("#111111", "#FFFFFF")} //block color
+        color={useColorModeValue("#FFFFFF", "#111111")} //the alphabet color inside block
+        minH={"60px"}
         py={{ base: 2 }}
         px={{ base: 4 }}
         borderBottom={1}
-        borderStyle={'solid'}
-        borderColor={useColorModeValue('#111111', '#FFFFFF')} // border color of the block
-        align={'center'}
+        borderStyle={"solid"}
+        borderColor={useColorModeValue("#111111", "#FFFFFF")} // border color of the block
+        align={"center"}
         justify="space-between"
       >
         {/* Left side with logo */}
-        <Flex flex={{ base: 1 }} justify={{ base: 'left', md: 'start' }} align="center">
+        <Flex
+          flex={{ base: 1 }}
+          justify={{ base: "left", md: "start" }}
+          align="center"
+        >
           <Box boxSize="auto">
-            <Image src="/icons/deva.png" alt="DevA" htmlHeight="30px" htmlWidth="120.54px" padding="15px"/>
+            <Image
+              src="/icons/deva.png"
+              alt="DevA"
+              htmlHeight="30px"
+              htmlWidth="120.54px"
+              padding="15px"
+            />
           </Box>
         </Flex>
 
         {/* Hamburger Icon (visible on smaller screens) */}
-        <Flex display={{ base: 'flex', md: 'none' }} align="center" pr={4}>
+        <Flex display={{ base: "flex", md: "none" }} align="center" pr={4}>
           <IconButton
             onClick={onToggle}
+            _hover={{}}
+            _active={{}}
             icon={
               isOpen ? (
-                <CloseIcon w={20} h={20} color="#FFFFFF" />  // This color of close icon when zoom in
+                <CloseIcon boxSize="24px" color="#FFFFFF" /> // This color of close icon when zoom in
               ) : (
-                <HamburgerIcon w={20} h={20} color="#FFFFFF" />  // This color of hamburger icon when zoom in
+                <HamburgerIcon boxSize="24px" color="#FFFFFF" /> // This color of hamburger icon when zoom in
               )
             }
-            variant={'ghost'}
-            aria-label={'Toggle Navigation'}
+            variant={"ghost"}
+            aria-label={"Toggle Navigation"}
           />
         </Flex>
 
         {/* Right side with navigation links (visible on larger screens) */}
-        <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
+        <Flex display={{ base: "none", md: "flex" }} ml={10}>
           <DesktopNav />
         </Flex>
       </Flex>
@@ -97,26 +119,26 @@ const Navbar: React.FC = () => {
 };
 
 const DesktopNav = () => {
-  const linkColor = useColorModeValue('#FFFFFF', '#750E21');  // navbar label element color (not zoom-in)
-  const linkHoverColor = useColorModeValue('#FF3434', '#1640D6'); // navbar label element color when hover (not zoom-in) 
-  const popoverContentBgColor = useColorModeValue('#0E121E', '#706233'); // block when hover label element
+  const linkColor = useColorModeValue("#FFFFFF", "#750E21"); // navbar label element color (not zoom-in)
+  const linkHoverColor = useColorModeValue("#FF3434", "#1640D6"); // navbar label element color when hover (not zoom-in)
+  const popoverContentBgColor = useColorModeValue("#0E121E", "#706233"); // block when hover label element
 
   return (
-    <Stack direction={'row'} spacing={4}>
+    <Stack direction={"row"} spacing={4}>
       {NAV_ITEMS.map((navItem) => (
         <Box key={navItem.label}>
-          <Popover trigger={'hover'} placement={'bottom-start'}>
+          <Popover trigger={"hover"} placement={"bottom-start"}>
             <PopoverTrigger>
               <Box
                 as="a"
                 p={2}
-                href={navItem.href ?? '#'}
-                fontSize={'16px'}
+                href={navItem.href ?? "#"}
+                fontSize={"16px"}
                 fontWeight={500}
                 color={linkColor}
                 padding={"15px"}
                 _hover={{
-                  textDecoration: 'none',
+                  textDecoration: "none",
                   color: linkHoverColor,
                 }}
               >
@@ -125,7 +147,14 @@ const DesktopNav = () => {
             </PopoverTrigger>
 
             {navItem.children && (
-              <PopoverContent border={0} boxShadow={'xl'} bg={popoverContentBgColor} p={4} rounded={'xl'} minW={'sm'}>
+              <PopoverContent
+                border={0}
+                boxShadow={"xl"}
+                bg={popoverContentBgColor}
+                p={4}
+                rounded={"xl"}
+                minW={"sm"}
+              >
                 <Stack>
                   {navItem.children.map((child) => (
                     <DesktopSubNav key={child.label} {...child} />
@@ -145,34 +174,35 @@ const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
     <Box
       as="a"
       href={href}
-      role={'group'}
-      display={'block'}
+      role={"group"}
+      display={"block"}
       p={2}
-      rounded={'md'}
-      _hover={{ bg: useColorModeValue('#073869', 'gray.900') }} //This is highlight Color when hover sub label inside the box
+      rounded={"md"}
+      _hover={{ bg: useColorModeValue("#073869", "gray.900") }} //This is highlight Color when hover sub label inside the box
     >
-      <Stack direction={'row'} align={'center'}>
+      <Stack direction={"row"} align={"center"}>
         <Box>
           <Text
-            transition={'all .3s ease'}
-            fontSize = {'16px'}
-            _groupHover={{ color: useColorModeValue('#FF3535', 'gray.900') }} // This is alphabet Color when hover sub label inside the box 
+            transition={"all .3s ease"}
+            fontSize={"16px"}
+            _groupHover={{ color: useColorModeValue("#FF3535", "gray.900") }} // This is alphabet Color when hover sub label inside the box
             fontWeight={500}
           >
             {label}
           </Text>
-          <Text fontSize={'14px'}>{subLabel}</Text>
+          <Text fontSize={"14px"}>{subLabel}</Text>
         </Box>
         <Flex
-          transition={'all .3s ease'}
-          transform={'translateX(-10px)'}
+          transition={"all .3s ease"}
+          transform={"translateX(-10px)"}
           opacity={0}
-          _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
-          justify={'flex-end'}
-          align={'center'}
+          _groupHover={{ opacity: "100%", transform: "translateX(0)" }}
+          justify={"flex-end"}
+          align={"center"}
           flex={1}
         >
-          <Icon color={'#FFFFFF'} w={5} h={5} as={ChevronRightIcon} /> {/*Color of icon right > when zoom out*/}
+          <Icon color={"#FFFFFF"} w={5} h={5} as={ChevronRightIcon} />{" "}
+          {/*Color of icon right > when zoom out*/}
         </Flex>
       </Stack>
     </Box>
@@ -181,7 +211,17 @@ const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
 
 const MobileNav = () => {
   return (
-    <Stack bg={useColorModeValue('#111111', 'gray.800')} p={4} display={{ md: 'none' }} fontSize={'16px'}> {/*Block color when zoom in (it's a menu block!!)*/}
+    <Stack
+      bg={useColorModeValue("#111111", "gray.800")}
+      px={8}
+      pb={4}
+      display={{ md: "none" }}
+      fontSize={"16px"}
+      borderBottom="1px solid"
+      borderColor="content.primary"
+    >
+      {" "}
+      {/*Block color when zoom in (it's a menu block!!)*/}
       {NAV_ITEMS.map((navItem) => (
         <MobileNavItem key={navItem.label} {...navItem} />
       ))}
@@ -197,40 +237,44 @@ const MobileNavItem = ({ label, children, href, subLabel }: NavItem) => {
       <Box
         py={2}
         as="a"
-        href={href ?? '#'}
+        href={href ?? "#"}
         justifyContent="space-between"
         alignItems="center"
         _hover={{
-          textDecoration: 'none',
+          textDecoration: "none",
         }}
       >
         <Flex>
-        <Text fontWeight={600} color={useColorModeValue('#FFFFFF', 'gray.200')}> {/*This is an alphabet color for label when zoom in*/}
-          {label}
-        </Text>
-        
-        {children && (
-          <Icon
-            as={ChevronDownIcon}
-            color="#FFFFFF"  /*Color of icon right > when zoom in*/
-            transition={'all .25s ease-in-out'}
-            transform={isOpen ? 'rotate(180deg)' : ''}
-            
-            w={6}
-            h={6}
-          />
-        )}
+          <Text
+            fontWeight={600}
+            color={useColorModeValue("#FFFFFF", "gray.200")}
+          >
+            {" "}
+            {/*This is an alphabet color for label when zoom in*/}
+            {label}
+          </Text>
+
+          {children && (
+            <Icon
+              as={ChevronDownIcon}
+              color="#FFFFFF" /*Color of icon right > when zoom in*/
+              transition={"all .25s ease-in-out"}
+              transform={isOpen ? "rotate(180deg)" : ""}
+              w={6}
+              h={6}
+            />
+          )}
         </Flex>
       </Box>
 
-      <Collapse in={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
+      <Collapse in={isOpen} animateOpacity style={{ marginTop: "0!important" }}>
         <Stack
           mt={2}
           pl={4}
           borderLeft={1}
-          borderStyle={'solid'}
-          borderColor={useColorModeValue('#FF3535', 'gray.700')} //this is line color
-          align={'start'}
+          borderStyle={"solid"}
+          borderColor={useColorModeValue("#FF3535", "gray.700")} //this is line color
+          align={"start"}
         >
           {children &&
             children.map((child) => (
@@ -239,33 +283,39 @@ const MobileNavItem = ({ label, children, href, subLabel }: NavItem) => {
                 key={child.label}
                 py={2}
                 href={child.href}
-                color={useColorModeValue('#FFFFFF', 'gray.700')} // This is a text color sublabel + subsublabel when zoom in
-                display={'block'}
-                role={'group'}
-                _hover={{ bg: useColorModeValue('#073869', 'gray.900') }} //This is a highlight color sublabel when hover and zoom in
+                color={useColorModeValue("#FFFFFF", "gray.700")} // This is a text color sublabel + subsublabel when zoom in
+                display={"block"}
+                role={"group"}
+                _hover={{ bg: useColorModeValue("#073869", "gray.900") }} //This is a highlight color sublabel when hover and zoom in
               >
-                <Stack direction={'row'} align={'center'}>
+                <Stack direction={"row"} align={"center"}>
                   <Box>
                     <Text
-                      transition={'all .3s ease'}
-                      fontSize = {'16px'}
-                      _groupHover={{ color: useColorModeValue('#FF3535', 'gray.900') }} //This is text color sublable when hover and zoom in
+                      transition={"all .3s ease"}
+                      fontSize={"16px"}
+                      _groupHover={{
+                        color: useColorModeValue("#FF3535", "gray.900"),
+                      }} //This is text color sublable when hover and zoom in
                       fontWeight={500}
                     >
                       {child.label}
                     </Text>
-                    <Text fontSize={'14px'}>{child.subLabel}</Text>
+                    <Text fontSize={"14px"}>{child.subLabel}</Text>
                   </Box>
                   <Flex
-                    transition={'all .3s ease'}
-                    transform={'translateX(-10px)'}
+                    transition={"all .3s ease"}
+                    transform={"translateX(-10px)"}
                     opacity={0}
-                    _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
-                    justify={'flex-end'}
-                    align={'center'}
+                    _groupHover={{
+                      opacity: "100%",
+                      transform: "translateX(0)",
+                    }}
+                    justify={"flex-end"}
+                    align={"center"}
                     flex={1}
                   >
-                    <Icon color={'#FFFFFF'} w={5} h={5} as={ChevronRightIcon} />  {/*Color of icon right > when zoom in*/}
+                    <Icon color={"#FFFFFF"} w={5} h={5} as={ChevronRightIcon} />{" "}
+                    {/*Color of icon right > when zoom in*/}
                   </Flex>
                 </Stack>
               </Box>
@@ -287,26 +337,26 @@ export default Navbar;
 
 const NAV_ITEMS: Array<NavItem> = [
   {
-    label: 'Project',
-    href: '#',
+    label: "Project",
+    href: "/project",
   },
   {
-    label: 'Founder',
+    label: "Founder",
     children: [
       {
-        label: 'sublabel1',
-        subLabel: 'subsublabel1',
-        href: '#',
+        label: "sublabel1",
+        subLabel: "subsublabel1",
+        href: "#",
       },
       {
-        label: 'sublabel2',
-        subLabel: 'subsublabel2',
-        href: '#',
+        label: "sublabel2",
+        subLabel: "subsublabel2",
+        href: "#",
       },
     ],
   },
   {
-    label: 'Contact',
-    href: '#',
+    label: "Contact",
+    href: "#",
   },
 ];
